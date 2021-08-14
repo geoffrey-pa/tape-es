f2073abcd0a90edbdb4cc84153a24ca4be7e4219
Geoffrey <geoffrey.pappaert@gmail.com>
2021-08-14 11:55:01 +0200

    added cli flag to only test git changed files

--- bin/tape-es.js
+++ bin/tape-es.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 import cli from 'commander'
+import gitChangedFiles from 'git-changed-files'
 import { runAll } from '../src/runners.js'
 import { match, readPkg } from '../src/util/index.js'
 
 const DEFAULT_PATTERN = '**/*.spec.js'
 const DEFAULT_IGNORE = '**/node_modules/**'
 const DEFAULT_ROOT = process.cwd()
-const DEFAULT_THREADS = 10;
+const DEFAULT_THREADS = 10
+const DEFAULT_GIT_CHANGES_ONLY = false
 
-(async () => {
+;(async () => {
   const pkg = await readPkg()
 
   cli.version(pkg.version)
@@ -16,14 +18,24 @@ const DEFAULT_THREADS = 10;
     .option('-i, --ignore [value]', 'Ignore files pattern')
     .option('-r, --root [value]', 'The root path')
     .option('-t, --threads [number]', 'Number of threads to run tests concurrently', parseInt)
+    .option('-g, --git-changes-only', 'Only process files changes since last git commit')
     .parse(process.argv)
 
   const pattern = cli.args[0] ? cli.args[0] : DEFAULT_PATTERN
   const ignore = cli.ignore ? cli.ignore : DEFAULT_IGNORE
   const root = cli.root ? cli.root : DEFAULT_ROOT
   const threads = cli.threads ? cli.threads : DEFAULT_THREADS
+  const gitChangesOnly = cli.gitChangesOnly ? cli.gitChangesOnly : DEFAULT_GIT_CHANGES_ONLY
 
-  const tests = await match(pattern, ignore, root)
+  let tests = await match(pattern, ignore, root)
+  if (gitChangesOnly) {
+    const { committedFiles, unCommittedFiles } = await gitChangedFiles()
+    const changedFiles = [...committedFiles, ...unCommittedFiles]
+    tests = tests.filter(file => {
+      return file.replace('.test.mjs', '.mjs').indexOf(changedFiles) === -1 || file.indexOf(changedFiles) === -1
+    })
+  }
+
   await runAll(tests, threads, root)
 })().catch(e => {
   console.error(e)
